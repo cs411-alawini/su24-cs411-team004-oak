cb54eb0902085f2ec8deaf4c4f84e0f8e43f782d
Nikhil Thomas <nikhilthomas@wirelessprv-10-194-153-8.near.illinois.edu>
2024-07-29 02:54:10 -0500

    Functionality to add to watchlist added

--- Project/main.py
+++ Project/main.py
@@ -335,6 +335,27 @@ def get_dashboard_balance(portfolios):
             total_balance += portfolio['PortfolioBalance']
     return total_balance
 
+def add_stock_to_watchlist(stock_symbol, portfolioid):
+    connection = None
+    try:
+        user_id = session['user']
+        
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO Watchlist (StockSymbol, PortfolioID)
+            VALUES (%s, %s)
+        """, (stock_symbol, portfolioid))
+        return True
+    except Error as e:
+        print(f"watchlist addition Error: {e}")
+    finally:
+        if connection and connection.is_connected():
+            connection.commit()
+            cursor.close()
+            connection.close()
+    return False
+
 """random user to test with"""
 # user: aaron16
 # password: ho^_7BnNS^
@@ -408,14 +429,17 @@ def portfolio_page(portfolioid):
     print(portfolio_data)
     return render_template('portfolio.html', transactions=transaction_data, watchlist=watchlist_data, portfolio=portfolio_data, sbalance=sbalance)
 
-@app.route('/add_watchlist', methods=['GET', 'POST'])
-def add_watchlist():
+@app.route('/portfolio/<portfolioid>/add_watchlist', methods=['GET', 'POST'])
+def add_watchlist(portfolioid):
     if request.method == 'POST':
+
         stock_symbol = request.form['stock_symbol']
+        stock_symbol = stock_symbol.upper()
         exist, name = get_stock_name_from_symbol(stock_symbol)
         if exist:
-            
-    return render_template('create_portfolio.html')
+            add_stock_to_watchlist(stock_symbol, portfolioid)
+            return redirect(url_for('portfolio_page',  portfolioid=portfolioid))
+    return render_template('add_watchlist.html', portfolioid=portfolioid)
 
 @app.route('/transactions/<portfolioid>', methods=['GET'])
 def transaction_page(portfolioid):
@@ -468,6 +492,7 @@ def remove_watch():
         print(f"{stock_symbol=}")
         print(f"{portfolio_id=}")
         remove_from_watch(portfolio_id, stock_symbol)
+    return redirect(url_for('portfolio_page', portfolioid=portfolio_id))
 
 
 
@@ -477,7 +502,7 @@ def buy_stock(portfolioid):
         return redirect(url_for('login'))
     
     stock_name = ''
-    stock_name = Decimal(0)
+    current_price = Decimal(0)
 
     if request.method == 'POST':
         if 'confirm_stock' in request.form:
@@ -487,7 +512,6 @@ def buy_stock(portfolioid):
 
             if exists:
                 current_price = get_stock_current_price(stock_symbol)
-            
 
             print("confirm stock")
         elif 'calculate_price' in request.form:
@@ -496,6 +520,7 @@ def buy_stock(portfolioid):
             print("place order")
 
     return render_template('buy_stock.html', portfolioid=portfolioid, msg=stock_name, stock_price=current_price)
+
 
 def get_stock_current_price(stock_symbol):
     try:
@@ -511,10 +536,6 @@ def get_stock_current_price(stock_symbol):
         return current_price
     except Exception as e:
         print("Error with yfinance API:", str(e))
-
-
-
-
 
 
 def get_stock_name_from_symbol(stock_symbol):
@@ -539,27 +560,6 @@ def get_stock_name_from_symbol(stock_symbol):
             cursor.close()
             connection.close()
 
-
-
-# https://www.geeksforgeeks.org/how-to-use-flask-session-in-python-flask/
-# @app.route("/logout")
-# def logout():
-#     session["name"] = None
-#     return redirect("/")
-    
-# @app.route('/db-test')
-# def db_test():
-#     connection = None
-#     try:
-#         connection = get_db_connection()
-#         if connection.is_connected():
-#             db_info = connection.get_server_info()
-#             return f'Connected to MySQL database... MySQL Server version: {db_info}'
-#     except Error as e:
-#         return f'Error while connecting to MySQL: {e}'
-#     finally:
-#         if connection and connection.is_connected():
-#             connection.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
