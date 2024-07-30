c2dc4a2695d15678378d66240ff62a7bd66002a6
lileddz <zacke2285@gmail.com>
2024-07-30 09:49:53 -0500

    hi lo from stored procedure

--- Project/main.py
+++ Project/main.py
@@ -273,6 +273,8 @@ def get_stats_performers(date_start, date_end):
     return False
 
 
+
+
 def get_fullname(userid):
     connection = None
     try:
@@ -350,7 +352,7 @@ def get_watchlist_data(portfolioid):
 
         user_id = session['user']
 
-        hi_lo_dict = get_hi_lo(user_id, portfolioid)
+        hi_lo_dict = get_hi_lo_sp(portfolioid)
 
         print("this is hi lo:", hi_lo_dict)
 
@@ -703,7 +705,7 @@ def get_stock_name_from_symbol(stock_symbol):
 
 
 
-def get_hi_lo(user_id, portfolio_id):
+def get_hi_lo(portfolio_id):
     connection = None
     try:
         connection = get_db_connection()
@@ -716,11 +718,34 @@ def get_hi_lo(user_id, portfolio_id):
             JOIN UserPortfolio up ON u.UserID = up.UsersUserID
             JOIN Watchlist w ON up.PortfoliosPortfolioID = w.PortfolioID
             JOIN HistoricalStocks hs ON w.StockSymbol = hs.StockSymbol
-            WHERE u.UserID = %s 
-                AND w.PortfolioID = %s
+            WHERE w.PortfolioID = %s
                 AND hs.Date >= DATE_SUB(CURDATE(), INTERVAL 30 DAY)
             GROUP BY u.UserID, w.StockSymbol
-                """, (user_id, portfolio_id))
+                """, (portfolio_id))
+        stock_hi_lo = []
+        for row in cursor:
+            stock_hi_lo.append(row)
+
+        hi_lo_dict = {}
+        for symbol, high, low in stock_hi_lo:
+            hi_lo_dict[symbol] = (high, low)
+
+        return hi_lo_dict
+    
+    except Error as e:
+        print(f"get stock name from symbol Error: {e}")
+    finally:
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+def get_hi_lo_sp(portfolio_id):
+    connection = None
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("CALL watchlistMinMax(%s)", (portfolio_id,))
         stock_hi_lo = []
         for row in cursor:
             stock_hi_lo.append(row)
